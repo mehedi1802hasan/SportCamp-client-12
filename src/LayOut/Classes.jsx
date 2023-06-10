dc711b97650c5d357a7347750303074bafc65977
mehedi1802hasan <mehedi1802hasan@gmail.com>
2023-06-11 04:45:04 +0600

    added myselectedClasses

--- src/LayOut/Classes.jsx
+++ src/LayOut/Classes.jsx
@@ -1,94 +1,124 @@
-import React from 'react';
-import { useState } from 'react';
-import { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import Swal from 'sweetalert2';
-//import { useContext } from 'react';
-//import { AuthContext } from '../Firebase/Provider';
+import { AuthContext } from '../Firebase/Provider';
+import { useContext } from 'react';
 const Classes = () => {
-  
+  const [classes, setClasses] = useState([]);
+const {user}=useContext(AuthContext)
+  useEffect(() => {
+    fetch('http://localhost:5000/classes/:instructorEmail')
+      .then((res) => res.json())
+      .then((data) => {
+        const filteredClasses = data.filter(
+          (classItem) => classItem.status === 'approved'
+        );
 
-   // const { user } = useContext(AuthContext)
-    const [classes, setClasses] = useState([])
-    useEffect(() => {
-        fetch('http://localhost:5000/classes/:instructorEmail')
-            .then((res) => res.json())
-            .then((data) => {
-                const filteredClasses = data.filter(
-                    (classItem) => classItem.status === 'approved'
-                );
-                setClasses(filteredClasses);
-                console.log(filteredClasses);
+        const disabledClasses = JSON.parse(localStorage.getItem('disabledClasses')) || [];
 
-            });
-    }, []);
-    const handleSelect =(cls)=>{
-        const selectedClass={image:cls.image ,className:cls.className , instructorName:cls.instructorName ,price:cls.price}
-        console.log(selectedClass)
-        fetch('http://localhost:5000/myselectedclass',{
-          method:"POST",
-          headers:{
-            'content-type':'application/json'
-          },
-          body:JSON.stringify(selectedClass)
-        })
-        .then(res=>res.json())
-        .then(data =>{
-          if(data.insertedId){
-            Swal.fire({
-              title: 'GOOD!',
-              text: 'posted successfully !!',
-              icon: 'success',
-              confirmButtonText: 'Okay'
-            })
-        }
+        const updatedClasses = filteredClasses.map((classItem) => {
+          if (disabledClasses.includes(classItem.className)) {
+            return { ...classItem, disabled: true };
+          } else {
+            return classItem;
+          }
+        });
+
+        setClasses(updatedClasses);
+      });
+  }, []);
+
+  const handleSelect = (cls) => {
+    const selectedClass = {
+        studentEmail:user.email,
+      image: cls.image,
+      className: cls.className,
+      instructorName: cls.instructorName,
+      price: cls.price,
+      seat: cls.seat
+    };
+
+    fetch('http://localhost:5000/myselectedclass', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(selectedClass)
     })
-   
-    }
-    return (
-        <div>
-        <div>
-        <h3 className='my-5 font-semibold text-center text-red-700 '>Total Clasess:  {classes.length}</h3>
-        <div className='overflow-x-auto'>
-            <table className='table w-full bg-yellow-500'>
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Image</th>
-                        <th>Class-Name</th>
-                        <th>Instructor-Name</th>
-                         <th>Avail-Seat</th>
-                        <th>Price</th>
-                        <th>Add for Booking</th>
-                    
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.insertedId) {
+          Swal.fire({
+            title: 'GOOD!',
+            text: 'Posted successfully!',
+            icon: 'success',
+            confirmButtonText: 'Okay'
+          });
 
-                    </tr>
-                </thead>
-                <tbody className='bg-green-500'>
-                    {classes.length > 0 &&
-                        classes.map((cls, i) => (
-                            <tr key={cls._id}>
-                                <th>{i + 1}</th>
-                                <td><img className='w-20 h-20' src={cls.image} alt="" /></td>
-                                <td>{cls.className}</td>
-                                <td>{cls.instructorName}</td>
-                                <td>{cls.seat}</td>
-                                <td>$ {cls.price} USD</td>
-                                <td ><button onClick={() => handleSelect(cls)} className="btn btn-sm btn-outline">Select</button>
-</td>
+          setClasses((prevClasses) =>
+            prevClasses.map((classItem) =>
+              classItem.className === cls.className
+                ? { ...classItem, disabled: true }
+                : classItem
+            )
+          );
 
-                            
-                                
-                               
-                                
+          const disabledClasses = JSON.parse(localStorage.getItem('disabledClasses')) || [];
+          localStorage.setItem(
+            'disabledClasses',
+            JSON.stringify([...disabledClasses, cls.className])
+          );
+        }
+      });
+  };
 
-                            </tr>
-                        ))}
-                </tbody>
-            </table>
+  return (
+    <div>
+      <div>
+        <h3 className="my-5 font-semibold text-center text-red-700">
+          Total Classes: {classes.length}
+        </h3>
+        <div className="overflow-x-auto">
+          <table className="table w-full bg-yellow-500">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Image</th>
+                <th>Class-Name</th>
+                <th>Instructor-Name</th>
+                <th>Avail-Seat</th>
+                <th>Price</th>
+                <th>Add for Booking</th>
+              </tr>
+            </thead>
+            <tbody className="bg-green-500">
+              {classes.length > 0 &&
+                classes.map((cls, i) => (
+                  <tr key={cls._id}>
+                    <th>{i + 1}</th>
+                    <td>
+                      <img className="w-20 h-20" src={cls.image} alt="" />
+                    </td>
+                    <td>{cls.className}</td>
+                    <td>{cls.instructorName}</td>
+                    <td>{cls.seat}</td>
+                    <td>$ {cls.price} USD</td>
+                    <td>
+                      <button
+                        onClick={() => handleSelect(cls)}
+                        className="btn btn-sm btn-outline"
+                        disabled={cls.disabled}
+                      >
+                        {cls.disabled ? 'Selected' : 'Select'}
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
         </div>
+      </div>
     </div>
-</div>
-    );
+  );
 };
 
 export default Classes;
--- src/Pages/Dashboard/MySelectedClasses.jsx
+++ src/Pages/Dashboard/MySelectedClasses.jsx
@@ -1,10 +1,69 @@
 import React from 'react';
-
+import { useContext } from 'react';
+import { AuthContext } from '../../Firebase/Provider';
+import { useState } from 'react';
+import { useEffect } from 'react';
 const MySelectedClasses = () => {
+    const { user } = useContext(AuthContext)
+    const [classes, setClasses] = useState([])
+    useEffect(() => {
+        fetch(`http://localhost:5000/myselectedclass/${user.email}`)
+            .then((res) => res.json())
+            .then((data) => {
+                const filteredClasses = data.filter(
+                    (classItem) => classItem.studentEmail === user.email
+                );
+                setClasses(filteredClasses);
+                console.log(filteredClasses);
+
+            });
+    }, [user.email]);
     return (
         <div>
-            My seceted Classs..............
+        <div>
+            <h3 className='my-5 font-semibold text-center text-red-700 '>My selected Clasess:  {classes.length}</h3>
+            <div className='overflow-x-auto'>
+                <table className='table w-full bg-yellow-500'>
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Image</th>
+                            <th>Class-Name</th>
+                            <th>Instructor-Name</th>
+                             <th>Avail-Seat</th>
+                            <th>Price</th>
+                            <th>Pay</th>
+                            <th> Delete</th>
+                        
+
+                        </tr>
+                    </thead>
+                    <tbody className='bg-green-500'>
+                        {classes.length > 0 &&
+                            classes.map((cls, i) => (
+                                <tr key={cls._id}>
+                                    <th>{i + 1}</th>
+                                    <td><img className='w-20 h-20' src={cls.image} alt="" /></td>
+                                  <td>{cls.className}</td>
+                                 <td>{cls.instructorName}</td>
+                                <td>{cls.seat}</td>
+                               <td>$ {cls.price} USD</td>
+                        <td><button className="btn btn-sm btn-outline">Pay</button></td>
+                       <td ><button className="btn btn-sm btn-outline">Delete</button></td>
+
+                                    
+                                
+                                    
+                                   
+                                    
+
+                                </tr>
+                            ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
+    </div>
     );
 };
 
