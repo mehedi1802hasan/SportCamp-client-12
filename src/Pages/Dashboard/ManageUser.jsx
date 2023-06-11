318ba85202323a14269571d89809ce8143f7d84a
mehedi1802hasan <mehedi1802hasan@gmail.com>
2023-06-11 22:21:52 +0600

    manageUser delete btn working properly

--- src/Pages/Dashboard/ManageUser.jsx
+++ src/Pages/Dashboard/ManageUser.jsx
@@ -31,27 +31,57 @@ const ManageUser = () => {
 
   const handleInstructor = (user) => {
     fetch(`http://localhost:5000/users/instructor/${user._id}`, {
-        method: 'PATCH',
+      method: 'PATCH',
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.modifiedCount) {
+          refetch();
+          Swal.fire({
+            title: 'SUCCESS!',
+            text: `${user.name} is now an Admin`,
+            icon: 'success',
+            confirmButtonText: 'Okay',
+          });
+        }
       })
-        .then((res) => res.json())
-        .then((data) => {
-          if (data.modifiedCount) {
-            refetch();
-            Swal.fire({
-              title: 'SUCCESS!',
-              text: `${user.name} is now an Admin`,
-              icon: 'success',
-              confirmButtonText: 'Okay',
-            });
-          }
-        })
-        .catch((error) => {
-          console.log('Error:', error);
-        });
+      .catch((error) => {
+        console.log('Error:', error);
+      });
   };
 
   const handleDelete = (user) => {
-    // Add logic to handle the delete button click here
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+
+        fetch(`http://localhost:5000/users/${user._id}`, {
+          method: 'DELETE'
+        })
+          .then(res => res.json())
+          .then(data => {
+
+            if (data.deletedCount > 0) {
+              refetch();
+              Swal.fire(
+                'Deleted!',
+                'User has been deleted.',
+                'success'
+              )
+            }
+          })
+
+
+
+      }
+    })
   };
 
   return (
