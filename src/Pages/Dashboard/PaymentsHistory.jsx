1940e38bd43172f549d935f596ffa0bb85907a36
mehedi1802hasan <mehedi1802hasan@gmail.com>
2023-06-15 21:14:20 +0600

    data decending by sort

--- src/Pages/Dashboard/PaymentsHistory.jsx
+++ src/Pages/Dashboard/PaymentsHistory.jsx
@@ -2,25 +2,26 @@ import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../Firebase/Provider';
 
 const PaymentsHistory = () => {
-    const { user } = useContext(AuthContext)
-    const [MyEnrolled, setMyEnrolled] = useState([])
+    const { user } = useContext(AuthContext);
+    const [MyEnrolled, setMyEnrolled] = useState([]);
+
     useEffect(() => {
         fetch('https://sport-camp-server.vercel.app/payments')
             .then(res => res.json())
-            .then((data) => {
-                const filteredClasses = data.filter(
-                    (classItem) => classItem.email === user.email
-                );
-                setMyEnrolled(filteredClasses);
-                console.log(filteredClasses);
-
+            .then(data => {
+                const filteredClasses = data.filter(classItem => classItem.email === user.email);
+                const sortedClasses = filteredClasses.sort((a, b) => new Date(b.date) - new Date(a.date));
+                setMyEnrolled(sortedClasses);
             });
-    }, [user.email])
+    }, [user.email]);
+
     return (
         <div>
             <div>
                 <div>
-                    <h3 className='my-5 font-semibold text-center text-red-700 '>My Enrolled Clasess:  {MyEnrolled.length}</h3>
+                    <h3 className='my-5 font-semibold text-center text-red-700'>
+                        My Paymented Classes: {MyEnrolled.length}
+                    </h3>
                     <div className='overflow-x-auto'>
                         <table className='table w-full bg-yellow-500'>
                             <thead>
@@ -28,12 +29,9 @@ const PaymentsHistory = () => {
                                     <th>#</th>
                                     <th>Image</th>
                                     <th>Class-Name</th>
-                                    <th>TransictionID</th>
+                                    <th>TransactionID</th>
                                     <th>Price</th>
                                     <th>Date</th>
-
-
-
                                 </tr>
                             </thead>
                             <tbody className='bg-green-500'>
@@ -41,19 +39,21 @@ const PaymentsHistory = () => {
                                     MyEnrolled.map((cls, i) => (
                                         <tr key={cls._id}>
                                             <th>{i + 1}</th>
-                                            <td><img className='w-20 h-20' src={cls.image} alt="" /></td>
+                                            <td>
+                                                <img className='w-20 h-20' src={cls.image} alt='' />
+                                            </td>
                                             <td>{cls.className}</td>
                                             <td>{cls.transactionId}</td>
                                             <td>$ {cls.price} USD</td>
                                             <td> {cls.date} </td>
-
                                         </tr>
                                     ))}
                             </tbody>
                         </table>
                     </div>
                 </div>
-            </div>        </div>
+            </div>
+        </div>
     );
 };
 
